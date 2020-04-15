842a60134d604089a5131a4304310c58c07c4512
Christopher Souza <cfsouza.ufal@gmail.com>
2020-04-15 10:26:55 -0300

    Fixed ListView class inheritance, object_list checking and db restarting

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic.list import ListView
 from .models import Post
 
 # Create your views here.
