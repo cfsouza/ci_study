d4fdacac4093d4edce6f5dc16bb0b3435bd63273
Christopher Souza <cfsouza.ufal@gmail.com>
2020-05-02 21:37:39 -0300

    Audits models via django-reversion

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
+from reversion.admin import VersionAdmin
+
 from .models import Post
 
 # Register your models here.
+# admin.site.unregister(Post)
 
+@admin.register(Post)
+class PostAdmin(VersionAdmin):
 
-admin.site.register(Post)
+    pass
+# admin.site.register(Post)
